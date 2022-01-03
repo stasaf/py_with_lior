87cfd016104fa9f1b5a13e6503a38d1aef9de6a9
astern <astern@bmc.com>
2022-01-03 22:22:39 +0200

    asaf

--- loop_continue.py
+++ loop_continue.py
@@ -1,11 +1,13 @@
+#continue is break point in the loop when if condition didnt full.
+
 maxcount = int(input("enter # to maximum calculate:\n"))
 x = 0
 total = 0
 
 while x < maxcount:
     x += 1
-    if x%2!=0:
+    if x%2!=0: # using Modolu to check if x is "mispar e-zugi"
         continue
-    total += x
+    total += x #sum all the "Misparin Zugiim" in the renge.
 
 print("Total sum of double numbers in range 0 -", maxcount, "is" , total)
