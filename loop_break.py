c788ddb1ed5fae114bebebdc8ba069b5c9a8e3e6
astern <astern@bmc.com>
2022-01-03 22:55:14 +0200

    class 30

---
+++ loop_break.py
@@ -0,0 +1,20 @@
+
+x = 0
+while True:
+    if x == 100:
+        break       #as long x<100 no break , if x=100 the loop will end
+    print ("x =",x)
+    x += 1
+
+print ("\n end of script..... \n\n\n")
+
+b = 0
+while b<150:
+    #if b == 100:
+     # break      
+    print ("b =",b)
+    b += 1
+else:
+    print(">>> loop didnt break!")
+    print(">>> b value is", b, "\n\n\n")
+print ("end of script... \n\n\n")
